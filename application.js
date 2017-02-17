6786f515b6ce8d63062c65356c9f90f8ac58a4ca
Leandro Dias <ledias.17@gmail.com>
2017-02-17 08:22:54 -0200

    using youtube api

--- application.js
+++ application.js
@@ -2,16 +2,25 @@ var form = $('#search-film');
 var search_field = $('#search-film-input');
 var results = $('#results');
 
+var KEY = 'AIzaSyBTo8mOJjxoqYSXEkdIZQdz07ldQndgKaA';
+
 form.on('submit', function(e) {
   e.preventDefault();
 
-  var url_search = 'http://www.omdbapi.com/?s=' + search_field.val();
-  $.get(url_search, function(response) {
+  var url_search = 'https://www.googleapis.com/youtube/v3/search';
+
+  var searchParams = {
+      part: 'id,snippet',
+      q: search_field.val(),
+      key: KEY
+  };
+
+  $.get(url_search, searchParams, function(response) {
     results.empty();
-    $.each(response.Search, function(index, item) {
+    $.each(response.items, function(index, item) {
       $('<div />')
-        .append($('<h4 />').html(item.Title))
-        .append($('<img />').attr('src', item.Poster))
+        .append($('<h4 />').html(item.snippet.title))
+        .append($('<img />').attr('src', item.snippet.thumbnails.high.url))
         .append($('<hr />'))
         .appendTo(results);
     });
